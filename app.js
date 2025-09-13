baeb86514f7c1a6acd1649ec636b33fee5b1ddd5
Luis Fuentes <luisangelfuentes24@gmail.com>
2025-09-13 17:33:15 -0500

    subiendo mi proyecto js parte 2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3127;
 
 app.use(express.json());
 
+
 let quotes = [
     "La vida es lo que sucede mientras estás ocupado haciendo otros planes. - John Lennon",
     "El éxito es la suma de pequeños esfuerzos repetidos día tras día. - Robert Collier",
@@ -13,23 +14,27 @@ let quotes = [
     "El futuro pertenece a quienes creen en la belleza de sus sueños. - Eleanor Roosevelt"
 ];  
 
-app.get("/convert/celsius-to-fahrenheit/:value", (req, res) => {
-    const celsius = parseFloat(req.params.value);
-    const fahrenheit = (celsius * 9/5) + 32;
-    return res.json({ fahrenheit: fahrenheit });
+app.get("/random/quotes", (req, res) => {
+    return res.json(quotes);
 });
 
-app.get("/convert/fahrenheit-to-celsius/:value", (req, res) => {
-    const fahrenheit = parseFloat(req.params.value);
-    const celsius = (fahrenheit - 32) * 5/9;
-    return res.json({ celsius: celsius });
+app.get("/random/quotes/random", (req, res) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return res.json({ quote: quotes[randomIndex] });
 });
 
-app.get("/convert/km-to-miles/:value", (req, res) => {
-    const km = parseFloat(req.params.value);
-    const miles = km * 0.621371;
-    return res.json({ miles: miles });
-});
+app.post("/random/quotes", (req, res) => {
+    const newQuote = req.body.quote;
+    if (newQuote) {
+        quotes.push(newQuote);
+        return res.status(201).json({ message: "Cita agregada", quote: newQuote });
+    } else {
+        return res.status(400).json({ error: "Cita no proporcionada" });
+    }
+}); 
+
+
+
 
 
 app.listen(port, () => {
